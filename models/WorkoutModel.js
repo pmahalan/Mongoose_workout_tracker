598fc3550bb2c78c9ae8ace8dd837d739b309369
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-01-29 20:48:34 -0500

    deleted an extraneous file

--- models/WorkoutModel.js
+++ models/WorkoutModel.js
@@ -3,40 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  // CODE HERE
-  workout_name: {
+  name: {
     type: String,
-    trim: true,
-    required: "A name for your workout is required!"
+    unique: true
   },
-
-  type: {
-    type: String,
-    trim: true
-  },
-
-   weight: {
-    type: Number,
-    trim: true
-  },
-
-  sets: {
-    type: Number,
-    trim: true
-  },
-
-reps: {
-  type: Number,
-  trim: true
-},
-
-duration: {
-  type: String,
-  trim: true,
-  required: "How long does this workout take?"
-}
-
-//if the workout is cardio, they should also be able to track the distance traveled
+  exercises: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Exercise"
+    }
+  ]
 
 });
 
