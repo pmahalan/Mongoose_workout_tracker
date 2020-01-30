32ad1261322359e8ddf0cd99b690d4d8140e477a
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-01-30 14:26:07 -0500

    completley changed the model structure...

--- models/WorkoutModel.js
+++ models/WorkoutModel.js
@@ -3,17 +3,54 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true
-  },
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
+    day: {
+      type: Date
+    },
+
+    exercises: [
+      {  
+      type: {
+        type: String,
+        trim: true
+      },
+      //cardio will be one of the types
+      //idk what other types are... intensity? resistance? weight?
+    
+      name: {
+        type: String,
+        trim: true,
+        required: "A name for this type of exercise is required!"
+      },
+
+      duration: {
+        type: String,
+        trim: true,
+        required: "How long does this exercise take?"
+      },
+
+      weight: {
+        type: Number,
+        trim: true
+      },
+
+      reps: {
+        type: Number,
+        trim: true
+      },
+    
+      sets: {
+        type: Number,
+        trim: true
+      },
+
+      distance: {
+        type: Number,
+        trim: true
+      }
     }
   ]
 
+//if the workout is cardio, they should also be able to track the distance traveled
 });
 
 const WorkoutModel = mongoose.model("Workout", WorkoutSchema);
