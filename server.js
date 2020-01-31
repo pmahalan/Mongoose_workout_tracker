d64a6a2c1bf7e245c6e6058a0f7e4c6e9aea7675
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-01-30 19:48:56 -0500

    tinkered with routes more

--- server.js
+++ server.js
@@ -19,31 +19,30 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 //___________________________
 
-// #### On the "htmlRoutes.js" file write code to:
-// -   Create the routes to open the application's HTML pages.
-// -   You will need at least three html routes to render:
+// -        HTML ROUTES       -
+
 // -       index.html ("/")
 app.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, "../Mongoose_workout_tracker/public/index.html"));
 });
 
 // -       exercise.html ("/exersice")
-  //html route to the page to create a new user
-  app.get('/exercise', function (req, res, next) {
-    res.sendFile(path.join(__dirname, "../Mongoose_workout_tracker/public/exercise.html"));
-  });
+app.get('/exercise', function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../Mongoose_workout_tracker/public/exercise.html"));
+}); 
 
 // -       stats.html ("/stats")
-  //html route to page with form for creating new event
-  app.get('/stats', function (req, res, next) {
-    res.sendFile(path.join(__dirname, "../Mongoose_workout_tracker/public/stats.html"));
-  });
+app.get('/stats', function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../Mongoose_workout_tracker/public/stats.html"));
+});
 
-// #### On the "apiRoutes.js" file write code to:
-// -   Create routes to handle the HTTP requests from the client and return the information from the database to the client.
-// -   You will need at least four api routes:
+//___________________________
 
-// -       get ("/api/workouts") - to Read all documents from the database. (done)
+// -        API ROUTES       -
+
+// To handle the HTTP requests from the client and return the information from the database to the client.
+
+// -       get ("/api/workouts") - to Read all documents from the database. (done!!!)
 app.get("/api/workouts", (req, res) => {
   db.WorkoutModel.find({})
     .then(dbFitness => {
@@ -54,23 +53,31 @@ app.get("/api/workouts", (req, res) => {
     });
 });
 
-// -       post ("/api/workouts") - to Create a Workout Document.
+// -       post ("/api/workouts") - to Create a Workout Document. 
+
+// This doesn't seem to be working. I should seek help.
+
+// Shouldn't this particular route be connected to something on the front-end? I.e. input fields and a submit button???
 app.post("/api/workouts", ({body}, res) => {
   db.WorkoutModel.create(body)
-  // ExerciseModel above refers to the name of my module.exports in the model
-    .then(({_id}) => db.WorkoutModel.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
-    // exercises above refers to the attribute of the WorkoutModel model.
-    .then(dbFitness => {
-      res.json(dbFitness);
+    .then(({_id}) => db.WorkoutModel.findOneAndUpdate({}, { $push: { workouts: _id }}, { new: true }))
+    .then(data => {
+      res.json(data);
     })
     .catch(err => {
       res.json(err);
     });
-});
+  })
 
 // -       update ("/api/workouts/:id") - to update (add) exercises to the workout document.
-// -       get ("/api/workouts/range") - to Read the last 7 documents (Workouts) from the collection to be displayed in the "stats.html" page.
+//          Just like adding books to the library. (Find the route that was responsible for that.)
+//          I DON'T KNOW HOW TO DO THIS, there were no update routes in any of the examples from day 1. 
+//          Maybe I should look at group project #2? I know we definitley used update there.
 
+
+
+// -       get ("/api/workouts/range") - to Read the last 7 documents (Workouts) from the collection to be displayed in the "stats.html" page.
+//          for this we'll probably refer to the first API route I hammered out above, then just filter it down to 7...
 
 
 
